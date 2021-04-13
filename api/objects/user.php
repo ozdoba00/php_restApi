14e978df1d440a7740e71e07df7729ea22539b60
Filip <ozdoba089@gmail.com>
2021-04-13 13:25:02 +0200

    User object with basic variables

--- api/objects/user.php
+++ api/objects/user.php
@@ -3,6 +3,20 @@
 //user object
 
 class User{
+    //db connection and table name
     private $conn;
     private $table_name = "users_jwt";
+
+    //object prop 
+
+    public $id;
+    public $firstname;
+    public $lastname;
+    public $email;
+    public $password;
+
+    public function __construct($db)
+    {
+        $this->conn = $db;
+    }
 }
